ff5c5147d28e4b5420b2baaba9c3a67380c09f85
Shashikumar <shashirgowdas67@gmail.com>
2022-02-19 16:35:40 +0530

    Added:Code to calculate the StopWatch number

---
+++ StopWatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical_Programs
+{
+    class Program6
+    {
+        public static void StopWatch()
+        {
+
+            long StartTimer;
+            long StopTimer;
+            Console.WriteLine("Press '1' to start time: ");
+            StartTimer = long.Parse(Console.ReadLine());
+            Console.WriteLine("Start Time is: " + StartTimer);
+            StartTimer = StartTimer * 1000;
+
+            Console.WriteLine("press '2' to stop time: ");
+            StopTimer = long.Parse(Console.ReadLine());
+            Console.WriteLine("Stop Time is: " + StopTimer);
+            StopTimer = StopTimer * 1000;
+            long elapsed = StopTimer - StartTimer;
+            Console.WriteLine("total elapsed time(in millisecond) is :" + elapsed);
+
+
+
+        }
+    }
+}
