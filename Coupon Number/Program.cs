6e69cb19c832eb0541fe1f8b9a0bd25c81cdaa08
Shashikumar <shashirgowdas67@gmail.com>
2022-02-19 16:30:52 +0530

    Added:Code to find coupon number in the program

---
+++ Coupon Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logical_Programs
+{
+    class Program
+    {
+        public static void RandomCoupon()
+        {
+            int distinct = 0, count = 0, couponNo;
+            Console.WriteLine("Enter a number of given distinct coupons:");
+            couponNo = int.Parse(Console.ReadLine());
+            bool[] isCollected = new bool[couponNo];
+            while (distinct < couponNo)
+            {
+                Random random = new Random();
+                int newCoupon = (int)(random.NextDouble() * couponNo);
+                count++;
+                if (!isCollected[newCoupon])
+                {
+                    distinct++;
+                    isCollected[newCoupon] = true;
+                }
+            }
+
+            Console.WriteLine("Total random number needed to have all distinct coupons:" + count);
+
+
+        }
+    }
+}
